9cd83559c750878ca6ab2bb0abe9d9d15e1a461a
Joel <joelgerman671@gmail.com>
2024-09-01 00:43:14 -0400

    Sync input of form with the own object

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
+// Selectores
 const pacienteInput = document.querySelector('#paciente');
 const propietarioInput = document.querySelector('#propietario');
 const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
+
+
+
+// Eventos 
+pacienteInput.addEventListener('change', datosCita);
+propietarioInput.addEventListener('change', datosCita);
+emailInput.addEventListener('change', datosCita);
+fechaInput.addEventListener('change', datosCita);
+sintomasInput.addEventListener('change', datosCita);
+
+
+
+// Objeto de citas
+const citaObj = {
+    paciente: '',
+    propietario: '',
+    email: '',
+    fecha: '',
+    sintomas: ''
+}
+
+
+function datosCita(e) { 
+    // De esta forma se insertara dinamicamente el valor de los inputs a la propiedades del objecto
+    citaObj[e.target.name] = e.target.value 
+    console.log(citaObj)
+}
 
