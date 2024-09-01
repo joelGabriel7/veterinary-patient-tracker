ffd9a16127277794ba1180874f26fd533f694688
Joel <joelgerman671@gmail.com>
2024-09-01 00:21:29 -0400

    catch of selectors of inputs

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const pacienteInput = document.querySelector('#paciente');
+const propietarioInput = document.querySelector('#propietario');
+const emailInput = document.querySelector('#email');
+const fechaInput = document.querySelector('#fecha');
+const sintomasInput = document.querySelector('#sintomas');
+
