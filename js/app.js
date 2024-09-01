a207a1513f1f293b1d739550f7ad3e43e0861582
Joel <joelgerman671@gmail.com>
2024-09-01 01:09:57 -0400

    Validation form, and create alert to emptys form then to insert to HTML by scripting

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
 
-
+const formulario = document.querySelector('#formulario-cita');
 
 // Eventos 
 pacienteInput.addEventListener('change', datosCita);
@@ -13,6 +13,9 @@ propietarioInput.addEventListener('change', datosCita);
 emailInput.addEventListener('change', datosCita);
 fechaInput.addEventListener('change', datosCita);
 sintomasInput.addEventListener('change', datosCita);
+
+formulario.addEventListener('submit',submitCita)
+
 
 
 
@@ -29,6 +32,47 @@ const citaObj = {
 function datosCita(e) { 
     // De esta forma se insertara dinamicamente el valor de los inputs a la propiedades del objecto
     citaObj[e.target.name] = e.target.value 
-    console.log(citaObj)
 }
 
+
+function submitCita(e) {
+    e.preventDefault();
+    if (Object.values(citaObj).some(valor => valor.trim() === '')) {
+        const notificacion = new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
+        notificacion.mostrar();
+        return;
+    }
+};
+
+
+class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto= texto,
+        this.tipo = tipo
+    }
+
+    mostrar(){
+        
+        // Crear la notificacion
+        
+        const alerta = document.createElement('div');
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm');
+
+        // Si la notificacion es de error!
+        
+        this.tipo === 'error' 
+        ? alerta.classList.add('bg-red-500') 
+        : alerta.classList.add('bg-green-500');
+        
+        // Mensaje de notificacion
+        alerta.textContent = this.texto
+
+        // insertar en el DOM
+        formulario.parentElement.insertBefore(alerta,formulario)
+    
+    }
+}
