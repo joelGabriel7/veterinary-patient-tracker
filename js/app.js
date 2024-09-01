4cf1c16ed277ed96ca26a06892fbf19187d18db1
Joel <joelgerman671@gmail.com>
2024-09-01 02:01:27 -0400

    adding button edit and delete

--- js/app.js
+++ js/app.js
@@ -58,62 +58,78 @@ class Notificacion {
 
         // Quitar despues de 5 segundos
         setTimeout(() => {
-           alerta.remove() 
+            alerta.remove()
         }, 3000);
 
     }
 }
 
 class AdminCitas {
-    constructor(){
+    constructor() {
         this.citas = []
         console.log(this.citas)
     }
 
-    agregar(cita){
-       this.citas = [...this.citas, cita];
-       this.mostrar();
+    agregar(cita) {
+        this.citas = [...this.citas, cita];
+        this.mostrar();
     }
 
     mostrar() {
         // Limpiar HTML
-        while(contenedorCita.firstChild) {
+        while (contenedorCita.firstChild) {
             contenedorCita.removeChild(contenedorCita.firstChild)
         }
 
         // Generando la cita
         this.citas.forEach(cita => {
             const divCita = document.createElement('div');
-            divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10' ,'rounded-xl', 'p-3');
-        
+            divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl', 'p-3');
+
             const paciente = document.createElement('p');
             paciente.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             paciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${cita.paciente}`;
-        
+
             const propietario = document.createElement('p');
             propietario.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             propietario.innerHTML = `<span class="font-bold uppercase">Propietario: </span> ${cita.propietario}`;
-        
+
             const email = document.createElement('p');
             email.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             email.innerHTML = `<span class="font-bold uppercase">E-mail: </span> ${cita.email}`;
-        
+
             const fecha = document.createElement('p');
             fecha.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             fecha.innerHTML = `<span class="font-bold uppercase">Fecha: </span> ${cita.fecha}`;
-        
+
             const sintomas = document.createElement('p');
             sintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             sintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${cita.sintomas}`;
-        
+
+            // Botones eliminar y editar 
+
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            const contenedorBotones = document.createElement('div');
+            contenedorBotones.classList.add('flex', 'justify-between','mt-10')
+            contenedorBotones.appendChild(btnEditar)
+            contenedorBotones.appendChild(btnEliminar)
+
             // Agregar al HTML
             divCita.appendChild(paciente);
             divCita.appendChild(propietario);
             divCita.appendChild(email);
             divCita.appendChild(fecha);
             divCita.appendChild(sintomas);
+            divCita.appendChild(contenedorBotones);
             contenedorCita.appendChild(divCita);
-        });      
+        });
     }
 }
 
@@ -134,14 +150,14 @@ function submitCita(e) {
         notificacion.mostrar();
         return;
     }
-    cita.agregar({...citaObj})
+    cita.agregar({ ...citaObj })
     const notificacion = new Notificacion({
         texto: 'Paciente Registrado',
         tipo: 'exito'
     })
 
     notificacion.mostrar();
-    formulario.reset()  
+    formulario.reset()
     reinciarObjectoCita()
 };
 
@@ -155,11 +171,11 @@ function reinciarObjectoCita() {
     'Es lo mismo'
 
     Object.assign(citaObj, {
-        paciente : '',
-        propietario : '',
-        email : '',
-        fecha : '',
-        sintomas : ''
+        paciente: '',
+        propietario: '',
+        email: '',
+        fecha: '',
+        sintomas: ''
     })
 
 }
